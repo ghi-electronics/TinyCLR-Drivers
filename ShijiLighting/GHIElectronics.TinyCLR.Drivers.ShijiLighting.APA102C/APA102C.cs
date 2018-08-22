2fd641eaa06508e23a73c33224eb01949ad32e56
Experiment-626 <aron.phillips@ghielectronics.com>
2018-08-22 08:05:40 -0400

    Adding initial base driver project to GHIElectronics.TinyCLR.Drivers for the Shiji Lighting APA102C addressable LEDs.

---
+++ ShijiLighting/GHIElectronics.TinyCLR.Drivers.ShijiLighting.APA102C/APA102C.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace GHIElectronics.TinyCLR.Drivers.ShijiLighting.APA102C {
+    public class APA102C {
+    }
+}
