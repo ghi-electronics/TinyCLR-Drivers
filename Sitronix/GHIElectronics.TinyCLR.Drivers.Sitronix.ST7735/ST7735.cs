4495504da111264e65c1cb5d12b128f5537bdb35
Experiment-626 <aron.phillips@ghielectronics.com>
2018-08-27 15:17:29 -0400

    Renamed WriteData(byte data) to SendData(byte data) and made the method public.

--- Sitronix/GHIElectronics.TinyCLR.Drivers.Sitronix.ST7735/ST7735.cs
+++ Sitronix/GHIElectronics.TinyCLR.Drivers.Sitronix.ST7735/ST7735.cs
@@ -78,71 +78,71 @@ namespace GHIElectronics.TinyCLR.Drivers.Sitronix.ST7735 {
 
             // ST7735R Frame Rate
             this.SendCommand(0xB1);
-            this.WriteData(0x01); this.WriteData(0x2C); this.WriteData(0x2D);
+            this.SendData(0x01); this.SendData(0x2C); this.SendData(0x2D);
             this.SendCommand(0xB2);
-            this.WriteData(0x01); this.WriteData(0x2C); this.WriteData(0x2D);
+            this.SendData(0x01); this.SendData(0x2C); this.SendData(0x2D);
             this.SendCommand(0xB3);
-            this.WriteData(0x01); this.WriteData(0x2C); this.WriteData(0x2D);
-            this.WriteData(0x01); this.WriteData(0x2C); this.WriteData(0x2D);
+            this.SendData(0x01); this.SendData(0x2C); this.SendData(0x2D);
+            this.SendData(0x01); this.SendData(0x2C); this.SendData(0x2D);
 
             this.SendCommand(0xB4); // Column inversion 
-            this.WriteData(0x07);
+            this.SendData(0x07);
 
             // ST7735R Power Sequence
             this.SendCommand(0xC0);
-            this.WriteData(0xA2); this.WriteData(0x02); this.WriteData(0x84);
-            this.SendCommand(0xC1); this.WriteData(0xC5);
+            this.SendData(0xA2); this.SendData(0x02); this.SendData(0x84);
+            this.SendCommand(0xC1); this.SendData(0xC5);
             this.SendCommand(0xC2);
-            this.WriteData(0x0A); this.WriteData(0x00);
+            this.SendData(0x0A); this.SendData(0x00);
             this.SendCommand(0xC3);
-            this.WriteData(0x8A); this.WriteData(0x2A);
+            this.SendData(0x8A); this.SendData(0x2A);
             this.SendCommand(0xC4);
-            this.WriteData(0x8A); this.WriteData(0xEE);
+            this.SendData(0x8A); this.SendData(0xEE);
 
             this.SendCommand(0xC5); // VCOM 
-            this.WriteData(0x0E);
+            this.SendData(0x0E);
 
             this.SendCommand(0x36); // MX, MY, RGB mode
-            this.WriteData(MADCTL_MX | MADCTL_MY | MADCTL_BGR);
+            this.SendData(MADCTL_MX | MADCTL_MY | MADCTL_BGR);
 
             // ST7735R Gamma Sequence
             this.SendCommand(0xe0);
-            this.WriteData(0x0f); this.WriteData(0x1a);
-            this.WriteData(0x0f); this.WriteData(0x18);
-            this.WriteData(0x2f); this.WriteData(0x28);
-            this.WriteData(0x20); this.WriteData(0x22);
-            this.WriteData(0x1f); this.WriteData(0x1b);
-            this.WriteData(0x23); this.WriteData(0x37); this.WriteData(0x00);
+            this.SendData(0x0f); this.SendData(0x1a);
+            this.SendData(0x0f); this.SendData(0x18);
+            this.SendData(0x2f); this.SendData(0x28);
+            this.SendData(0x20); this.SendData(0x22);
+            this.SendData(0x1f); this.SendData(0x1b);
+            this.SendData(0x23); this.SendData(0x37); this.SendData(0x00);
 
-            this.WriteData(0x07);
-            this.WriteData(0x02); this.WriteData(0x10);
+            this.SendData(0x07);
+            this.SendData(0x02); this.SendData(0x10);
             this.SendCommand(0xe1);
-            this.WriteData(0x0f); this.WriteData(0x1b);
-            this.WriteData(0x0f); this.WriteData(0x17);
-            this.WriteData(0x33); this.WriteData(0x2c);
-            this.WriteData(0x29); this.WriteData(0x2e);
-            this.WriteData(0x30); this.WriteData(0x30);
-            this.WriteData(0x39); this.WriteData(0x3f);
-            this.WriteData(0x00); this.WriteData(0x07);
-            this.WriteData(0x03); this.WriteData(0x10);
+            this.SendData(0x0f); this.SendData(0x1b);
+            this.SendData(0x0f); this.SendData(0x17);
+            this.SendData(0x33); this.SendData(0x2c);
+            this.SendData(0x29); this.SendData(0x2e);
+            this.SendData(0x30); this.SendData(0x30);
+            this.SendData(0x39); this.SendData(0x3f);
+            this.SendData(0x00); this.SendData(0x07);
+            this.SendData(0x03); this.SendData(0x10);
 
             this.SendCommand(0x2a);
-            this.WriteData(0x00); this.WriteData(0x00);
-            this.WriteData(0x00); this.WriteData(0x7f);
+            this.SendData(0x00); this.SendData(0x00);
+            this.SendData(0x00); this.SendData(0x7f);
             this.SendCommand(0x2b);
-            this.WriteData(0x00); this.WriteData(0x00);
-            this.WriteData(0x00); this.WriteData(0x9f);
+            this.SendData(0x00); this.SendData(0x00);
+            this.SendData(0x00); this.SendData(0x9f);
 
             this.SendCommand(0xF0); //Enable test command  
-            this.WriteData(0x01);
+            this.SendData(0x01);
             this.SendCommand(0xF6); //Disable ram power save mode 
-            this.WriteData(0x00);
+            this.SendData(0x00);
 
             this.SetColorFormat(ColorFormat.BGR16bit565); // Sets default color format to 65k color
 
             // Rotate
             this.SendCommand(ST7735_MADCTL);
-            this.WriteData(MADCTL_MV | MADCTL_MY);
+            this.SendData(MADCTL_MV | MADCTL_MY);
 
             this.SendCommand(0x29); //Display on
             Thread.Sleep(50);
@@ -154,7 +154,7 @@ namespace GHIElectronics.TinyCLR.Drivers.Sitronix.ST7735 {
             this.spiBus.Write(this.singleByteBuffer);
         }
 
-        private void WriteData(byte data) {
+        public void SendData(byte data) {
             this.singleByteBuffer[0] = data;
             this.WriteData(this.singleByteBuffer);
         }
@@ -188,12 +188,12 @@ namespace GHIElectronics.TinyCLR.Drivers.Sitronix.ST7735 {
                 case ColorFormat.BGR12bit444: // 4k colors mode 
                     this.bitsPerPixel = 12;
                     this.SendCommand(0x3A);
-                    this.WriteData(0x03);
+                    this.SendData(0x03);
                     break;
                 case ColorFormat.BGR16bit565: // 65k colors mode
                     this.bitsPerPixel = 16;
                     this.SendCommand(0x3A); 
-                    this.WriteData(0x05);
+                    this.SendData(0x05);
                     break;
             }
         }
