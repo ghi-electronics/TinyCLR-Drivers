fd8616d2d3f15b74310fd430784613f1a8f3cbf6
John Brochue <johnbrochue@outlook.com>
2018-09-26 14:14:10 -0400

    Updated versions

--- STMicroelectronics/SPWF04Sx/Properties/AssemblyInfo.cs
+++ STMicroelectronics/SPWF04Sx/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("365C4668-89FB-429C-B90B-D80AF6CF9B9D")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.10000")]
-[assembly: AssemblyInformationalVersion("1.0.0-preview1")]
+[assembly: AssemblyFileVersion("1.0.0.10001")]
+[assembly: AssemblyInformationalVersion("1.0.0-preview2")]
