6327675d8ab447a110443fceb2d542ffe379df00
Experiment-626 <aron.phillips@ghielectronics.com>
2018-10-26 13:33:42 -0400

    Adding preliminary driver files for CD4017B, a Texas Instruments CMOS counter/divider IC.

---
+++ TexasInstruments/CD4017B/GHIElectronics.TinyCLR.Drivers.TexasInstruments.CD4017B/CD4017B.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace GHIElectronics.TinyCLR.Drivers.TexasInstruments.CD4017B {
+    public class CD4017B {
+    }
+}
