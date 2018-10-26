82f5bd025d78ce26235b4ae8e8e4ba37f99cc33d
Experiment-626 <aron.phillips@ghielectronics.com>
2018-10-26 14:24:21 -0400

    Added code to the driver.

--- TexasInstruments/CD4017B/GHIElectronics.TinyCLR.Drivers.TexasInstruments.CD4017B/CD4017B.cs
+++ TexasInstruments/CD4017B/GHIElectronics.TinyCLR.Drivers.TexasInstruments.CD4017B/CD4017B.cs
@@ -1,9 +1,32 @@
-﻿using System;
-using System.Collections;
-using System.Text;
-using System.Threading;
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
 
 namespace GHIElectronics.TinyCLR.Drivers.TexasInstruments.CD4017B {
     public class CD4017B {
+        private readonly GpioPin clock;
+        private readonly GpioPin reset;
+        private int currentCount;
+
+        public CD4017B(int clockPin, int resetPin) {
+            var gpio = GpioController.GetDefault();
+            this.clock = gpio.OpenPin(clockPin);
+            this.clock.SetDriveMode(GpioPinDriveMode.Output);
+            this.reset = gpio.OpenPin(resetPin);
+            this.reset.SetDriveMode(GpioPinDriveMode.Output);
+            this.ResetCountToZero();
+        }
+
+        public void ResetCountToZero() {
+            this.reset.Write(GpioPinValue.High);
+            this.reset.Write(GpioPinValue.Low);
+            this.currentCount = 0;
+        }
+
+        public void IncreaseCount() {
+            this.clock.Write(GpioPinValue.High);
+            this.clock.Write(GpioPinValue.Low);
+            this.currentCount++;
+        }
+
+        public int GetCurrentCount() => this.currentCount;
     }
 }
