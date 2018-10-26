a3edf2e776f3f17df196942e49623f941b2e9606
Experiment-626 <aron.phillips@ghielectronics.com>
2018-10-26 14:45:34 -0400

    Adding preliminary driver files for SNx4HC595, a Texas Instruments 8-Bit Shift Registers IC w/ 3-State Output Registers.

---
+++ TexasInstruments/SNx4HC595/GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595/SNx4HC595.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595 {
+    public class SNx4HC595 {
+    }
+}
