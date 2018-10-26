841a96100005d1c2311f9cf26e37c57fbcc92b27
Experiment-626 <aron.phillips@ghielectronics.com>
2018-10-26 14:51:41 -0400

    Added code to the driver.

--- TexasInstruments/SNx4HC595/GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595/SNx4HC595.cs
+++ TexasInstruments/SNx4HC595/GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595/SNx4HC595.cs
@@ -1,9 +1,29 @@
-﻿using System;
-using System.Collections;
-using System.Text;
-using System.Threading;
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Devices.Spi;
 
 namespace GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595 {
     public class SNx4HC595 {
+        private readonly GpioPin latch;
+        private readonly SpiDevice spiBus;
+
+        public SNx4HC595(string spiName, int chipSelect, int latchPin) {
+            this.spiBus = SpiController.FromName(spiName).GetDevice(
+                new SpiConnectionSettings() {
+                    ChipSelectLine = chipSelect,
+                    ChipSelectType = SpiChipSelectType.Gpio,
+                    Mode = SpiMode.Mode0,
+                    ClockFrequency = 25_000_000,
+                    DataBitLength = 8
+                });
+
+            this.latch = GpioController.GetDefault().OpenPin(latchPin);
+            this.latch.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        public void WriteBuffer(byte[] buffer) {
+            this.latch.Write(GpioPinValue.Low);
+            this.spiBus.Write(buffer);
+            this.latch.Write(GpioPinValue.High);
+        }
     }
 }
