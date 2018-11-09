cbf86e8d27b00a5557b4e468580a783c5ad6abf2
Experiment-626 <aron.phillips@ghielectronics.com>
2018-11-09 11:52:40 -0500

    Changed spiName from string to SpiDevice and removed the controller code. Temporarily removed the configuration code to place in own method.

--- TexasInstruments/SNx4HC595/GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595/SNx4HC595.cs
+++ TexasInstruments/SNx4HC595/GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595/SNx4HC595.cs
@@ -7,15 +7,16 @@ namespace GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595 {
         private readonly GpioPin latch;
         private readonly SpiDevice spiBus;
 
-        public SNx4HC595(string spiName, int chipSelect, int latchPin) {
-            this.spiBus = SpiController.FromName(spiName).GetDevice(
-                new SpiConnectionSettings() {
-                    ChipSelectLine = chipSelect,
-                    ChipSelectType = SpiChipSelectType.Gpio,
-                    Mode = SpiMode.Mode0,
-                    ClockFrequency = 25_000_000,
-                    DataBitLength = 8
-                });
+        public SNx4HC595(SpiDevice spiBus, int chipSelect, int latchPin) {
+            this.spiBus = spiBus;
+            //this.spiBus = SpiController.FromName(spiName).GetDevice(
+            //    new SpiConnectionSettings() {
+            //        ChipSelectLine = chipSelect,
+            //        ChipSelectType = SpiChipSelectType.Gpio,
+            //        Mode = SpiMode.Mode0,
+            //        ClockFrequency = 25_000_000,
+            //        DataBitLength = 8
+            //    });
 
             this.latch = GpioController.GetDefault().OpenPin(latchPin);
             this.latch.SetDriveMode(GpioPinDriveMode.Output);
