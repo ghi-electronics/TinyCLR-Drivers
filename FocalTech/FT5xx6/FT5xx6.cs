070fc818602432f26c87c7cb8e91feb8f0dc37c9
Gus Issa <gusi@ghielectronics.com>
2019-03-02 10:45:35 -0500

    Fix up event and add gesture events
    Driver does not support multi touch.  Up/Down events need special processing to do multi touch.

--- FocalTech/FT5xx6/FT5xx6.cs
+++ FocalTech/FT5xx6/FT5xx6.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using GHIElectronics.TinyCLR.Devices.Gpio;
 using GHIElectronics.TinyCLR.Devices.I2c;
 
@@ -12,19 +12,28 @@ namespace GHIElectronics.TinyCLR.Drivers.FocalTech.FT5xx6 {
             this.Y = y;
         }
     }
+    public enum Gesture {
+        MoveUp = 0x1C,
+        MoveDown = 0x14,
+        MoveLeft = 0x10,
+        MoveRight = 0x18,
+        ZoomIn = 0x48,
+        ZoomOut = 0x49,
+    }
 
     public delegate void TouchEventHandler(FT5xx6Controller sender, TouchEventArgs e);
+    public delegate void GestureHandler(FT5xx6Controller sender, Gesture gesture);
 
     public class FT5xx6Controller : IDisposable {
         private readonly byte[] addressBuffer = new byte[1];
-        private readonly byte[] read4 = new byte[4];
-        private readonly byte[] read1 = new byte[1];
+        private readonly byte[] read7 = new byte[7];
         private readonly I2cDevice i2c;
         private readonly GpioPin interrupt;
 
         public event TouchEventHandler TouchDown;
         public event TouchEventHandler TouchUp;
         public event TouchEventHandler TouchMove;
+        public event GestureHandler OnGesture;
 
         public static I2cConnectionSettings GetConnectionSettings() => new I2cConnectionSettings(0x38) {
             BusSpeed = I2cBusSpeed.FastMode,
@@ -47,16 +56,16 @@ namespace GHIElectronics.TinyCLR.Drivers.FocalTech.FT5xx6 {
         }
 
         private void OnInterrupt(GpioPin sender, GpioPinValueChangedEventArgs e) {
-            var points = this.ReadData(2, this.read1)[0];
+            this.addressBuffer[0] = (byte)0;
 
-            for (var i = 0; i < points; i++) {
-                var data = this.ReadData(i * 6 + 3, this.read4);
-                var flag = (data[0] & 0xC0) >> 6;
-                var x = ((data[0] & 0x0F) << 8) | data[1];
-                var y = ((data[2] & 0x0F) << 8) | data[3];
+            this.i2c.WriteRead(this.addressBuffer, read7);
 
-                (flag == 0 ? this.TouchDown : flag == 1 ? this.TouchUp : flag == 2 ? this.TouchMove : null)?.Invoke(this, new TouchEventArgs(x, y));
-            }
+            if (read7[1] != 0) this.OnGesture?.Invoke(this, (Gesture)read7[1]);
+
+            var x = ((read7[3] & 0x0F) << 8) | read7[4];
+            var y = ((read7[5] & 0x0F) << 8) | read7[6];
+                var flag = (read7[3] & 0xC0) >> 6;
+            (flag == 0 ? this.TouchDown : flag == 1 ? this.TouchUp : flag == 2 ? this.TouchMove : null)?.Invoke(this, new TouchEventArgs(x, y));
         }
 
         private byte[] ReadData(int address, byte[] buffer) {
