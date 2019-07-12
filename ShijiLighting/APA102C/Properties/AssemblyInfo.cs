95edb2ab877d7099b8e601b4bd1862648309e7d9
Dat <dat.tran@ghielectronics.com>
2019-07-12 10:47:00 -0400

    Update 2.0.0-Prev1

--- ShijiLighting/APA102C/Properties/AssemblyInfo.cs
+++ ShijiLighting/APA102C/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2019")]
 [assembly: ComVisible(false)]
 [assembly: Guid("99624F01-2024-4C7E-809E-3717CC41F760")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.10003")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.10000")]
 [assembly: AssemblyInformationalVersion("1.0.0-rtw")]
--- SolomonSystech/SSD1306/Properties/AssemblyInfo.cs
+++ SolomonSystech/SSD1306/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2019")]
 [assembly: ComVisible(false)]
 [assembly: Guid("95DE8D49-631E-41EB-90F7-B53768803FE6")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.10003")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.10000")]
 [assembly: AssemblyInformationalVersion("1.0.0-rtw")]
