a9793e27c4af6abb8e0573e1c006e2261877e49a
Dat <dat.tran@ghielectronics.com>
2019-12-17 14:40:18 -0500

    Add vlsi mp3 decoder

---
+++ VlsiSolution/GHIElectronics.TinyCLR.Drivers.VlsiSolution.VS1053B/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.VlsiSolution.VS1053B")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2019")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8a352d82-167c-4e06-9bd8-b1c054471806")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.10000")]
+[assembly: AssemblyInformationalVersion("2.0.0-preview1")]
