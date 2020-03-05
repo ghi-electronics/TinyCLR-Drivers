7fbc83fab220afda21213e1789555867788024d5
Dat <dat.tran@ghielectronics.com>
2020-03-05 10:27:32 -0500

    use pin to clear. Change spi configuration.

--- TexasInstruments/SNx4HC595/GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595/SNx4HC595.cs
+++ TexasInstruments/SNx4HC595/GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595/SNx4HC595.cs
@@ -5,48 +5,52 @@ using GHIElectronics.TinyCLR.Devices.Spi;
 namespace GHIElectronics.TinyCLR.Drivers.TexasInstruments.SNx4HC595 {
     public class SNx4HC595 : IDisposable {
         private readonly GpioPin latch;
+        private readonly GpioPin reset;
         private readonly SpiDevice spiBus;
-        private byte[] invalidateBuffer;
 
-        public static SpiConnectionSettings GetSpiConnectionSettings() => new SpiConnectionSettings {
-            ChipSelectType = SpiChipSelectType.None,
-            Mode = SpiMode.Mode0,
-            ClockFrequency = 12_000_000,
-            DataBitLength = 8
-        };
+        public SNx4HC595(SpiController spiController, GpioPin latch, GpioPin reset, int clock = 12_000_000) {
+            var settings = new SpiConnectionSettings {
+                ChipSelectType = SpiChipSelectType.None,
+                Mode = SpiMode.Mode0,
+                ClockFrequency = clock,
+                DataBitLength = 8
+            };
 
-        public SNx4HC595(SpiDevice spiBus, GpioPin latch) {
-            this.spiBus = spiBus;
+            this.spiBus = spiController.GetDevice(settings);
 
             this.latch = latch;
             this.latch.SetDriveMode(GpioPinDriveMode.Output);
+
+            this.reset = reset;
+            this.reset.SetDriveMode(GpioPinDriveMode.Output);
         }
 
         public void Dispose() {
             this.latch.Dispose();
             this.spiBus.Dispose();
+            this.reset.Dispose();
         }
 
         public void WriteBuffer(byte[] buffer) {
             if (buffer == null) throw new ArgumentNullException(nameof(buffer));
 
-            if (this.invalidateBuffer == null || this.invalidateBuffer.Length != buffer.Length) {
-                this.invalidateBuffer = new byte[buffer.Length];
-
-                Array.Clear(this.invalidateBuffer, 0, this.invalidateBuffer.Length);
-            }
-
-            this.latch.Write(GpioPinValue.Low);
             this.spiBus.Write(buffer);
-            this.latch.Write(GpioPinValue.High);
+            this.Latch();
         }
 
-        public void Invalidate() {
-            if (this.invalidateBuffer != null) {
-                this.latch.Write(GpioPinValue.Low);
-                this.spiBus.Write(this.invalidateBuffer);
-                this.latch.Write(GpioPinValue.High);
-            }
+        public void Clear() {
+            this.Reset();
+            this.Latch();
+        }
+
+        private void Latch() {
+            this.latch.Write(GpioPinValue.High);
+            this.latch.Write(GpioPinValue.Low);
+        }
+
+        private void Reset() {
+            this.reset.Write(GpioPinValue.Low);
+            this.reset.Write(GpioPinValue.High);
         }
     }
 }
