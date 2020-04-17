72bb4f62cab7f733db290b2756f4353593e77d4e
Dat <dat.tran@ghielectronics.com>
2020-04-17 15:24:28 -0400

    add pca9685 driver

---
+++ Nxp/PCA9685/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.Nxp.PCA9685")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2020")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6E3E6107-F537-437B-86F3-04982098D4D1")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.50000")]
+[assembly: AssemblyInformationalVersion("2.0.0-preview5")]
+
