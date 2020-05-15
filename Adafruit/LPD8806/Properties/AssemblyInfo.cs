605fdd688b79c10877f8554c2fc760404420709b
Dat <dat.tran@ghielectronics.com>
2020-05-15 10:09:30 -0400

    to preview6

--- Adafruit/LPD8806/Properties/AssemblyInfo.cs
+++ Adafruit/LPD8806/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("6FE7815A-14AD-4D72-9C6A-496201A698DB")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.50000")]
-[assembly: AssemblyInformationalVersion("2.0.0-preview5")]
+[assembly: AssemblyFileVersion("2.0.0.60000")]
+[assembly: AssemblyInformationalVersion("2.0.0-preview6")]
--- Sitronix/ST7735/Properties/AssemblyInfo.cs
+++ Sitronix/ST7735/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("D945E7ED-82F5-4768-A41F-0EF0220E6BA6")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.50000")]
-[assembly: AssemblyInformationalVersion("2.0.0-preview5")]
+[assembly: AssemblyFileVersion("2.0.0.60000")]
+[assembly: AssemblyInformationalVersion("2.0.0-preview6")]
