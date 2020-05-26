c406125bb96590f342fe80ba59d7e47bd9b629a4
Dat <dat.tran@ghielectronics.com>
2020-05-26 18:11:37 -0400

    add silead gsl1680 (480x272)

---
+++ Silead/GSL1680/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.Silead.GSL1680")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2020")]
+[assembly: ComVisible(false)]
+[assembly: Guid("abc90ec4-0ba2-47a9-984a-b24e89eb00eb")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.60000")]
+[assembly: AssemblyInformationalVersion("2.0.0-preview6")]
