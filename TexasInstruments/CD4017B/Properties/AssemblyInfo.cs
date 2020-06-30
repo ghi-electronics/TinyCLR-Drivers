aa4c38ba02424e70566a3983d4a871d46ca7e177
Dat <dat.tran@ghielectronics.com>
2020-06-30 15:45:27 -0400

    correct folder

---
+++ TexasInstruments/CD4017B/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.TexasInstruments.CD4017B")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2020")]
+[assembly: ComVisible(false)]
+[assembly: Guid("620771fc-4f70-42e5-beb4-b69c8fcd6617")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.7000")]
+[assembly: AssemblyInformationalVersion("2.0.0-rc1")]
