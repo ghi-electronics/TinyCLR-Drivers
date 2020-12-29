5e1cf46d3bfc4004390ffe981a58a74ba3033b8c
Dat <dat.tran@ghielectronics.com>
2020-12-29 17:52:50 -0500

    Add servo moto TowerPro SG90 nuget

---
+++ TowerPro/SG90/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.TowerPro.SG90")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2020")]
+[assembly: ComVisible(false)]
+[assembly: Guid("FC977532-9E2F-4C23-9032-1DF0DA9EF78A")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.3000")]
+[assembly: AssemblyInformationalVersion("2.1.0-preview3")]
