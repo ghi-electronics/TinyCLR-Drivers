7a0ea4e61b659b98d9ff5f6067d93ac5cd1df9c2
Dat <dat.tran@ghielectronics.com>
2021-03-04 18:03:12 -0500

    Add resistive touch driver

---
+++ Touch/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.Touch.ResistiveTouch")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2021")]
+[assembly: ComVisible(false)]
+[assembly: Guid("F6472533-5F6B-4EEA-BE7D-BE52D0A5E79A")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.4000")]
+[assembly: AssemblyInformationalVersion("2.1.0-preview4")]
