9d430998eec20bde22c9ffc01bc5842d31ed30b0
Dat Tran <dat.tran@ghielectronics.com>
2021-03-19 13:51:30 -0400

    add Basic Graphic driver

---
+++ BasicGraphic/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.BasicGraphic")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2021")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8286622D-37E2-44C8-963A-90FD6957AC79")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.4000")]
+[assembly: AssemblyInformationalVersion("2.1.0-preview4")]
+
