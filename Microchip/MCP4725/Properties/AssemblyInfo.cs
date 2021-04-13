92d726b72957637e4f3f3f0e3bba084723e3a486
Dat Tran <dat.tran@ghielectronics.com>
2021-04-13 16:31:19 -0400

    Add MCP4725

---
+++ Microchip/MCP4725/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.Microchip.MCP4725")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2021")]
+[assembly: ComVisible(false)]
+[assembly: Guid("D18059CE-E835-447E-8D2C-00858F62638A")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.5000")]
+[assembly: AssemblyInformationalVersion("2.1.0-rc1")]
+
