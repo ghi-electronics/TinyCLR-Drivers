0c899c7b4d4900623604ddf0cdf2b79b9f76f3df
Dat Tran <dat.tran@ghielectronics.com>
2021-04-14 11:59:08 -0400

    add I2CI2cConnectionSettings

--- TexasInstruments/ADC121/ADC121.cs
+++ TexasInstruments/ADC121/ADC121.cs
@@ -6,29 +6,23 @@ using System.Threading;
 using GHIElectronics.TinyCLR.Devices.I2c;
 
 namespace GHIElectronics.TinyCLR.Drivers.TexasInstruments.ADC121 {
-    public class Adc121
-    {
+    public class Adc121 {
         private I2cDevice device;
 
         //The ADC result is located inside of the register with address 0x00
         private readonly byte[] sendBuffer = { 0x00 };
         private byte[] resultBuffer = new byte[2];
 
+        public static I2cConnectionSettings GetConnectionSettings(byte address) => new I2cConnectionSettings(address) {
+            BusSpeed = 100000,
+            AddressFormat = I2cAddressFormat.SevenBit,
+        };
 
         public Adc121(I2cDevice i2c) => this.device = i2c;
 
-        public int GetValue()
-        {
-            try
-            {
-                this.device.WriteRead(this.sendBuffer, this.resultBuffer);
-                return ((this.resultBuffer[0] << 8) + this.resultBuffer[1]);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("Something went wrong while communicating with the ADC121!");
-            }
-            return -1;
+        public int GetValue() {
+            this.device.WriteRead(this.sendBuffer, this.resultBuffer);
+            return (this.resultBuffer[0] << 8) + this.resultBuffer[1];
         }
     }
 }
