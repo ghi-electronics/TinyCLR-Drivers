6480aec4d893302679d596eadfd2d753fe22b281
Dat Tran <dat.tran@ghielectronics.com>
2021-05-21 14:40:30 -0400

    Rewrite WS2812 neopixel

--- Worldsemi/WS2812/WS2812.cs
+++ Worldsemi/WS2812/WS2812.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using GHIElectronics.TinyCLR.Devices.Gpio;
@@ -7,145 +8,27 @@ using GHIElectronics.TinyCLR.Devices.Signals;
 
 namespace GHIElectronics.TinyCLR.Drivers.Worldsemi.WS2812 {
     public class WS2812Controller {
-        private readonly DigitalSignal digitalSignalPin;
-        private readonly SignalGenerator signalGeneratorPin;
 
-        private readonly int numLeds;
-
-        private readonly uint[] bufferTimming;
-        private readonly TimeSpan[] timeSpanTimming;
-
-        private readonly bool digitalSignalMode;
-
-        public uint TimingHigh { get; set; }
-        public uint TimingLow { get; set; }
-
-        const int BYTE_PER_LED = 48; // 24 bit RGB  = 48 element
-
-        public WS2812Controller(DigitalSignal dataPin, int numLeds) {
-            this.digitalSignalPin = dataPin;
-            this.digitalSignalMode = true;
-            this.TimingHigh = 17;
-            this.TimingLow = 8;
+        private readonly GpioPin gpioPin;
+        private readonly int numLeds;        
+        private readonly byte[] data;        
+        public WS2812Controller(GpioPin dataPin, int numLeds) {
+            this.gpioPin = dataPin;
             this.numLeds = numLeds;
-            this.bufferTimming = new uint[1 + this.numLeds * BYTE_PER_LED ];
+            this.data = new byte[this.numLeds * 3];
 
-            // reset
-            // digitalSignal Reset command, 1000 * 2 * 50 (multiplier) = 100us            
-            this.bufferTimming[0] = 1000 * 2;
-
-            for (var i = 0; i < numLeds; i++)
-                this.SetColor(i, 0x00, 0x00, 0x00);
-
-            this.Flush();
+            this.gpioPin.SetDriveMode(GpioPinDriveMode.Output);
         }
 
-        public WS2812Controller(SignalGenerator dataPin, int numLeds) {
-            this.signalGeneratorPin = dataPin;
-            this.TimingHigh = 5;
-            this.TimingLow = 1;
-            this.numLeds = numLeds;
-            this.timeSpanTimming = new TimeSpan[1 + this.numLeds * BYTE_PER_LED];
-
-            this.signalGeneratorPin.DisableInterrupts = true;
-            this.signalGeneratorPin.IdleValue = GpioPinValue.Low;
-
-            // reset            
-            // signalgenerator Reset command, 100 * 10 () = 100us
-            this.timeSpanTimming[this.timeSpanTimming.Length - 1] = TimeSpan.FromTicks(100 * 10);
-
-            for (var i = 0; i < numLeds; i++)
-                this.SetColor(i, 0x00, 0x00, 0x00);
-
-            this.Flush();
+        public void SetColor(int index, byte red, byte green, byte blue) {
+            this.data[index * 3 + 0] = green;
+            this.data[index * 3 + 1] = red;
+            this.data[index * 3 + 2] = blue;
         }
 
-        public void SetColor(int index, int red, int green, int blue) {
-            var idx = 0;
-
-            if (this.digitalSignalMode) {
-                for (var i = 7; i >= 0; i--) {
-                    if ((green & (1 << i)) > 0) {
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 0 + 0 + idx] = this.TimingHigh;
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 0 + 1 + idx] = this.TimingLow;
-                    }
-                    else {
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 0 + 0 + idx] = this.TimingLow;
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 0 + 1 + idx] = this.TimingHigh; ;
-                    }
-
-                    if ((red & (1 << i)) > 0) {
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 16 + 0 + idx] = this.TimingHigh; ;
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 16 + 1 + idx] = this.TimingLow;
-                    }
-                    else {
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 16 + 0 + idx] = this.TimingLow;
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 16 + 1 + idx] = this.TimingHigh; ;
-                    }
-
-                    if ((blue & (1 << i)) > 0) {
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 32 + 0 + idx] = this.TimingHigh; ;
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 32 + 1 + idx] = this.TimingLow;
-                    }
-                    else {
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 32 + 0 + idx] = this.TimingLow;
-                        this.bufferTimming[1 + index * BYTE_PER_LED + 32 + 1 + idx] = this.TimingHigh; ;
-                    }
-
-                    idx += 2;
-
-                }
-            }
-            else {
-                for (var i = 7; i >= 0; i--) {
-                    if ((green & (1 << i)) > 0) {
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 0 + 0 + idx] = TimeSpan.FromTicks(this.TimingHigh);
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 0 + 1 + idx] = TimeSpan.FromTicks(this.TimingLow);
-                    }
-                    else {
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 0 + 0 + idx] = TimeSpan.FromTicks(this.TimingLow);
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 0 + 1 + idx] = TimeSpan.FromTicks(this.TimingHigh); ;
-                    }
-
-                    if ((red & (1 << i)) > 0) {
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 16 + 0 + idx] = TimeSpan.FromTicks(this.TimingHigh); ;
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 16 + 1 + idx] = TimeSpan.FromTicks(this.TimingLow);
-                    }
-                    else {
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 16 + 0 + idx] = TimeSpan.FromTicks(this.TimingLow);
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 16 + 1 + idx] = TimeSpan.FromTicks(this.TimingHigh); ;
-                    }
-
-                    if ((blue & (1 << i)) > 0) {
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 32 + 0 + idx] = TimeSpan.FromTicks(this.TimingHigh); ;
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 32 + 1 + idx] = TimeSpan.FromTicks(this.TimingLow);
-                    }
-                    else {
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 32 + 0 + idx] = TimeSpan.FromTicks(this.TimingLow);
-                        this.timeSpanTimming[0 + index * BYTE_PER_LED + 32 + 1 + idx] = TimeSpan.FromTicks(this.TimingHigh); ;
-                    }
-
-                    idx += 2;
-                }
-            }
-        }
-        
         public void Flush() {
-            // From 0....47 is first Led.
-            // From 48....95 is second Led
-            // ...
-            // Last element is 100us (must be at least 50us) reset which latches the colors
-            if (this.digitalSignalPin != null) {
-
-                while (this.digitalSignalPin.CanGenerate == false) {
-                    Thread.Sleep(1);
-                }
-
-                this.digitalSignalPin.Generate(this.bufferTimming, 0, (uint)this.bufferTimming.Length, 50);
-            }
-            else {
-                this.signalGeneratorPin.Write(this.timeSpanTimming);
-            }
+            this.Reset();
+            this.NativeFlush(this.gpioPin.PinNumber, this.data, 0, this.data.Length);
         }
 
         public void SetBuffer(byte[] buffer, int offset, int count) {
@@ -163,7 +46,7 @@ namespace GHIElectronics.TinyCLR.Drivers.Worldsemi.WS2812 {
                 var green = (((color) & 0x07E0) >> 5) << 2;
                 var blue = ((color) & 0x001F) << 3;
 
-                this.SetColor(led, red, green, blue);
+                this.SetColor(led, (byte)red, (byte)green, (byte)blue);
             }
         }
 
@@ -171,5 +54,15 @@ namespace GHIElectronics.TinyCLR.Drivers.Worldsemi.WS2812 {
             for (var i = 0; i < this.numLeds; i++)
                 this.SetColor(i, 0x00, 0x00, 0x00);
         }
+
+        private void Reset() {
+            this.gpioPin.Write(GpioPinValue.Low);
+
+            var expired = DateTime.Now.Ticks + 10 * 50 * 2;
+            while (DateTime.Now.Ticks < expired) ;
+        }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern void NativeFlush(int dataPin, byte[] buffer8, int offset, int size);
     }
 }
