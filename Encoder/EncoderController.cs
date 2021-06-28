e1a70ff1b567dcc066a42c32a57664d637cc8377
Dat Tran <dat.tran@ghielectronics.com>
2021-06-28 17:46:49 -0400

    Add Encoder driver

---
+++ Encoder/EncoderController.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+
+namespace GHIElectronics.TinyCLR.Drivers.Encoder {
+    public class EncoderController : IDisposable {
+        readonly GpioPin pinA;
+        readonly GpioPin pinB;
+        private int counter;
+        private bool run = false;
+
+        private TimeSpan timeout = TimeSpan.FromMilliseconds(100);
+
+        public delegate void CounterChangedEvent(int counter);
+        public event CounterChangedEvent OnCounterChangedEvent;
+
+        public EncoderController(GpioPin pinA, GpioPin pinB) {
+            this.pinA = pinA;
+            this.pinB = pinB;
+
+            this.pinA.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            this.pinB.SetDriveMode(GpioPinDriveMode.InputPullUp);
+
+            this.run = true;
+
+            new Thread(this.Process).Start();
+        }
+        private void Process() {
+            while (this.run) {
+
+                var valueA = this.pinA.Read() == GpioPinValue.High ? true : false;
+                var valueB = this.pinB.Read() == GpioPinValue.High ? true : false;
+
+                var expired = DateTime.Now + this.timeout;
+                if (valueB == false) {
+
+                    while (this.pinB.Read() == GpioPinValue.Low) {
+                        if (DateTime.Now >= expired)
+                            goto ignored;
+                    }
+
+                    if (this.pinA.Read() == GpioPinValue.Low) {
+                        this.counter++;
+
+                        OnCounterChangedEvent?.Invoke(this.counter);
+                    }
+
+                    continue;
+
+                }
+
+                if (valueA == false) {
+                    while (this.pinA.Read() == GpioPinValue.Low) {
+                        if (DateTime.Now >= expired)
+                            goto ignored;
+                    }
+
+                    if (this.pinB.Read() == GpioPinValue.Low) {
+                        this.counter--;
+
+                        OnCounterChangedEvent?.Invoke(this.counter);
+                    }
+                }
+
+            ignored:
+                Thread.Sleep(20);
+            }
+        }
+
+        public void Dispose() {
+            this.run = false;
+        }
+
+        public int Counter => this.counter;
+
+    }
+}
---
+++ Encoder/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.Encoder")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2021")]
+[assembly: ComVisible(false)]
+[assembly: Guid("826f2bea-28f6-46f9-aa7b-9e853551df24")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.7000")]
+[assembly: AssemblyInformationalVersion("2.1.0-rtw")]
