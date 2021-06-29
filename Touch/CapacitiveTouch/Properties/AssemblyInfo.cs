a3a9ca77ebd99375d925717c808bc91ff198348c
Dat Tran <dat.tran@ghielectronics.com>
2021-06-29 16:41:39 -0400

    Add CapacitiveTouch

---
+++ Touch/CapacitiveTouch/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.TinyCLR.Drivers.Touch.CapacitiveTouch")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyProduct("TinyCLR OS")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2021")]
+[assembly: ComVisible(false)]
+[assembly: Guid("F21D4105-57A9-454F-8E77-9C3AA86903F2")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.7000")]
+[assembly: AssemblyInformationalVersion("2.1.0-rtw")]
+
