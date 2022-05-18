65ca8614cc3d13584839bcb8e8a8e539a2403e33
Dat Tran <dat.tran@ghielectronics.com>
2022-05-18 11:11:28 -0400

    to v2.2.0.4200 (back from 5000)

--- Infrared/Properties/AssemblyInfo.cs
+++ Infrared/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2022")]
 [assembly: ComVisible(false)]
 [assembly: Guid("CA009AF8-232D-4FFE-AD8E-E0058AFA2F8C")]
-[assembly: AssemblyVersion("2.2.0.5000")]
-[assembly: AssemblyFileVersion("2.2.0.5000")]
-[assembly: AssemblyInformationalVersion("2.2.0.5000")]
+[assembly: AssemblyVersion("2.2.0.4200")]
+[assembly: AssemblyFileVersion("2.2.0.4200")]
+[assembly: AssemblyInformationalVersion("2.2.0.4200")]
 
